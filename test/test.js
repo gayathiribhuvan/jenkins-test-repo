76d0e87f50cad300818f97eb8de7e2f9e183c1db
Gayathiri Bhuvaneshwaran <gayathiribhuvaneshwaran@gmail.com>
2026-02-14 12:21:45 +0530

    Removed duplicate failing test

--- test/test.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-const add = require('../index');
-
-describe('Addition Function', function () {
-    it('should return 5 when adding 2 + 3', function () {
-        assert.strictEqual(add(2, 3), 5);
-    });
-});
